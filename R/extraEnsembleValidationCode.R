a0f0bc1c8d8be3c01a2967f48d3b8329f6935150
jreps <jreps@its.jnj.com>
2022-04-20 13:14:28 -0400

    Update extraEnsembleValidationCode.R
    updating code to validate for new PLP interface

--- R/extraEnsembleValidationCode.R
+++ R/extraEnsembleValidationCode.R
@@ -1,65 +1,80 @@
-# externally validate the models in the new data:
-options(andromedaTempFolder = "D:/andromedaTemp")
+# function to validate the models across other databases 
+validateModels <- function(
+  databaseDetails = list(
+    list(
+      name = 'ccae',
+      connectionDetailList = list(),
+      cdmDatabaseSchema = '',
+      tempEmulationSchema = '',
+      cohortDatabaseSchema = '',
+      cohortTable = '',
+      outcomeDatabaseSchema = '',
+      outcomeTable = ''
+      ),
+    ist(name = 'mdcd'),
+    ist(name = 'mdcr'),
+    ist(name = 'optumClaims'),
+    ist(name = 'optumEhr')
+  ),
+  modelLocation = 'D:/ensembleDatabase',
+  cohortTable = 'databaseEnsembleCohort'
+){
+  
+  databases <- unlist(lapply(databaseDetails, function(x) x$name))
 
-databases <- c('ccae','mdcd','mdcr','optumEhr','optumClaims')
 
-for(modelDatabase in databases){
+for(ind in 1:length(databases)){
+  
+  modelDatabase <- databases[ind]
 
   for(valDatabase in databases[databases!=modelDatabase]){
     
-    database <- valDatabase
-    # USER INPUTS
-    #=======================
-    # The folder where the study intermediate and result files will be written:
-    
-    connectionDetails <- DatabaseConnector::createConnectionDetails(dbms = dbms, 
-                                                                    user = user, 
-                                                                    password = pw, 
-                                                                    server = server, 
-                                                                    port = port,
-                                                                    extraSettings = extraSettings)
-    
-    # Add the database containing the OMOP CDM data
-    cdmDatabaseSchema <- cdmDatabaseSchema
-    # Add a sharebale name for the cdmDatabaseSchema database
-    databaseName <- cdmDatabaseName
-    # Add a database with read/write access as this is where the cohorts will be generated
-    cohortDatabaseSchema <- cohortDatabaseSchema
-    
-    oracleTempSchema <- NULL
-    
-    # table name where the cohorts will be generated
-    cohortTable <- 'databaseEnsembleCohort'
-    
+    connectionDetails <- do.call(
+      DatabaseConnector::createConnectionDetails, 
+      databaseDetails[[which(databases == valDatabase)]]$connectionDetailList
+      )
     
     for(analysisId in  1:21){
       
-      model <- PatientLevelPrediction::loadPlpResult(file.path('D:/ensembleDatabase',modelDatabase,paste0('Analysis_',analysisId), 'PlpResult'))
-      pop <- readRDS(file.path('D:/ensembleDatabase',valDatabase,paste0('StudyPop_L1_T8931_O',model$inputSetting$populationSettings$outcomeId,'_P1.rds')))
+      res <- PatientLevelPrediction::loadPlpResult(file.path(modelLocation,modelDatabase,paste0('Analysis_',analysisId), 'PlpResult'))
+      pop <- readRDS(file.path(modelLocation,valDatabase,paste0('StudyPop_L1_T8931_O',res$model$trainDetails$outcomeId,'_P1.rds')))
 
-      plpData <- PatientLevelPrediction::similarPlpData(plpModel = model$model, 
-                                                        newConnectionDetails = connectionDetails, 
-                                                        newCdmDatabaseSchema = cdmDatabaseSchema, 
-                                                        newCohortDatabaseSchema = cohortDatabaseSchema, 
-                                                        newCohortTable = cohortTable, 
-                                                        newCohortId = 8931, 
-                                                        newOutcomeDatabaseSchema = cohortDatabaseSchema, 
-                                                        newOutcomeTable = cohortTable, 
-                                                        newOutcomeId = model$inputSetting$populationSettings$outcomeId, 
-                                                        createPopulation = T)
+      plpData <- PatientLevelPrediction::getPlpData(
+        databaseDetails = PatientLevelPrediction::createDatabaseDetails(
+          connectionDetails = connectionDetails,
+          cdmDatabaseSchema = databaseDetails[[which(databases == valDatabase)]]$cdmDatabaseSchema,
+          cdmDatabaseName = valDatabase,
+          tempEmulationSchema = databaseDetails[[which(databases == valDatabase)]]$tempEmulationSchema,
+          cohortDatabaseSchema = databaseDetails[[which(databases == valDatabase)]]$cohortDatabaseSchema,
+          cohortTable = databaseDetails[[which(databases == valDatabase)]]$cohortTable,
+          outcomeDatabaseSchema = databaseDetails[[which(databases == valDatabase)]]$outcomeDatabaseSchema,
+          outcomeTable = databaseDetails[[which(databases == valDatabase)]]$outcomeTable,
+          cohortId = res$model$trainDetails$cohortId,
+          outcomeIds = res$model$trainDetails$outcomeId
+            ), 
+        covariateSettings =  res$model$trainDetails$covariateSettings, 
+        restrictPlpDataSettings = res$model$trainDetails$getPlpSettings
+      )
       
-      
-      cohort <- plpData$plpData$cohorts
+      cohort <- plpData$cohorts
       md <- attr(pop, 'metaData')
       newPop <- merge(pop[,colnames(pop)!='rowId'], cohort[,c('rowId', 'subjectId', 'cohortStartDate')], by = c('subjectId', 'cohortStartDate'))
       attr(newPop, 'metaData') <- md
-      exVal <- PatientLevelPrediction::applyModel(population = newPop, plpData = plpData$plpData, plpModel = model$model)
       
-      
-      if(!dir.exists(file.path('D:/ensembleDatabase',modelDatabase, 'validation',valDatabase,paste0('Analysis_',analysisId)))){
-        dir.create(file.path('D:/ensembleDatabase',modelDatabase, 'validation',valDatabase,paste0('Analysis_',analysisId)),recursive = T )
+      exVal <- list(
+        prediction = PatientLevelPrediction::predictPlp(
+          plpModel =  res$model, 
+          plpData = plpData, 
+          population = newPop
+        )
+      )
+
+      if(!dir.exists(file.path(modelLocation,modelDatabase, 'validation',valDatabase,paste0('Analysis_',analysisId)))){
+        dir.create(file.path(modelLocation,modelDatabase, 'validation',valDatabase,paste0('Analysis_',analysisId)),recursive = T )
       }
-      saveRDS(exVal, file.path('D:/ensembleDatabase',modelDatabase, 'validation',valDatabase,paste0('Analysis_',analysisId), 'validationResult.rds' ))
+      saveRDS(exVal, file.path(modelLocation,modelDatabase, 'validation',valDatabase,paste0('Analysis_',analysisId), 'validationResult.rds' ))
     }
   }
+}
+  
 }
